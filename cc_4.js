7ffe2c749c396ed64ec039fff041a5340ac7f627
Justin Wong <jcwong5@usf.edu>
2025-09-16 22:44:56 -0400

    Completed Step One

--- cc_4.js
+++ cc_4.js
@@ -1,3 +1,10 @@
-// Coding Challenge 00
+// Coding Challenge 04
 
-// Code goes here
+const product = [
+    {Name: "Water Bottle", Category: "Home Appliance", Price: 15.00, Inventory: 18},
+    {Name: "Apple", Category: "Groceries", Price: 2.00, Inventory: 25},
+    {Name: "Table", Category: "Home Decoration", Price: 25.00, Inventory: 8},
+    {Name: "Computer", Category: "Electronic", Price: 500.00, Inventory: 2},
+    {Name: "Bleach", Category: "Cleanning", Price: 12.00, Inventory: 7}
+
+];
