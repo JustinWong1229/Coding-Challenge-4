9581501d3c9deebd477b2677be7a188f74442405
Justin Wong <jcwong5@usf.edu>
2025-09-19 11:34:50 -0400

    Changed the categories of my products.

--- cc_4.js
+++ cc_4.js
@@ -1,10 +1,14 @@
 // Coding Challenge 04
 
-const product = [
-    {Name: "Water Bottle", Category: "Home Appliance", Price: 15.00, Inventory: 18},
-    {Name: "Apple", Category: "Groceries", Price: 2.00, Inventory: 25},
-    {Name: "Table", Category: "Home Decoration", Price: 25.00, Inventory: 8},
-    {Name: "Computer", Category: "Electronic", Price: 500.00, Inventory: 2},
-    {Name: "Bleach", Category: "Cleanning", Price: 12.00, Inventory: 7}
+let products = [
+    {Name: "T-shirt", Category: "apparel", Price: 15.00, Inventory: 18},
+    {Name: "Apple", Category: "groceries", Price: 2.00, Inventory: 25},
+    {Name: "Table", Category: "household", Price: 25.00, Inventory: 8},
+    {Name: "Computer", Category: "electronic", Price: 500.00, Inventory: 2},
+    {Name: "Bleach", Category: "household", Price: 12.00, Inventory: 7}
 
 ];
+console.log(products);
+
+// Step 3: Applying a dynamic discount
+
