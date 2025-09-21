95c426cb82111c7732fd278a7dc521dbfbc11b06
Justin Wong <jcwong5@usf.edu>
2025-09-20 22:13:09 -0400

    Kind of completed step 5, need to go back and make it to where each customer does not buy the same things.

--- cc_4.js
+++ cc_4.js
@@ -4,7 +4,7 @@ let products = [
     {Name: "T-shirt", Category: "apparel", Price: 15.00, Inventory: 18},
     {Name: "Apple", Category: "groceries", Price: 2.00, Inventory: 25},
     {Name: "Table", Category: "furniture", Price: 25.00, Inventory: 8},
-    {Name: "Computer", Category: "electronic", Price: 500.00, Inventory: 2},
+    {Name: "Computer", Category: "electronic", Price: 500.00, Inventory: 4},
     {Name: "Bleach", Category: "household", Price: 12.00, Inventory: 7}
 
 ];
@@ -35,21 +35,23 @@ for(let product of products) {
     
 }
 // Step 4 : Customer Types
-let customerType = "regular"
+const customerType = ["regular", "student", "senior"]
 let extraDiscount = 0;
 if (customerType === "student") {extraDiscount = 0.05;}
 else if (customerType === "senior") {extraDiscount = 0.07;}
 else {extraDiscount = 0;}
 
 // Step 5: Checkout Process and Inventory
+
 for (let index = 0; index < 3; index++) {
     let subTotal = 0
     for (const product of products) {
-
-        (item.Inventory--)
+        subTotal = subTotal + product.promoPrice;
+        if (product.Inventory>0)
+        (product.Inventory--)
     }
     let finalTotal = subTotal * (1 - extraDiscount);
-console.log(`Customer ${i}: $${finalTotal.toFixed(2)}`);
+console.log(`Customer ${index}: $${finalTotal.toFixed(2)}`);
 }
 
 
